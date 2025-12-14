b51e63e6c3b070695cddb6333c3ff00f21ecdcc7
Parsa Ahmadi Nasab Emran <parsatempleuniversity@gmail.com>
2025-12-14 12:44:06 -0500

    feat: add Book domain model

--- src/main/java/com/example/collections/domain/Book.java
+++ src/main/java/com/example/collections/domain/Book.java
@@ -1,4 +1,42 @@
 package com.example.collections.domain;
 
 public class Book {
+
+    private Long id;
+    private String isbn;
+    private String title;
+    private Float price;
+
+    public Book(Long id, String isbn, String title, Float price) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
